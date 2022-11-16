4ea63da129d85f30450684b96c2ee93041610504
supriyaDwarampudi <108483000+supriyaDwarampudi@users.noreply.github.com>
2022-11-16 12:06:23 +0530

    Update input type.py

--- input type.py
+++ input type.py
@@ -8,3 +8,19 @@ print(z)
 o/p:Enter first number9
   Enter first number11
   20
+  (or)
+  x=input('enter a number')
+a=int(x)
+y=input('enter b number')
+b=int(y)
+z=a+b
+print(z)
+o/p:
+  enter a number2
+enter b number3
+5
+ch=input('enter a char')
+print(ch[4])
+   o/p:
+   Enter a charSUPRIYA
+   i
