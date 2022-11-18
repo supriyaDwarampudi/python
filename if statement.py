59c066455c791c70fff984aaf846d3332350bf8d
supriya <supriya1213@sasi.ac.in>
2022-11-18 21:05:01 +0530

    if statement

---
+++ if statement.py
@@ -0,0 +1,3 @@
+if True:
+   print("I m false")
+   print("supriya")
--- input type.py
+++ input type.py
@@ -1,4 +1,2 @@
-x=int(input("Enter first number"))
-y=int(input("Enter second number"))
-z=x+y
-print(z)
+result=eval(input('Enter the exp'))
+print(result)
